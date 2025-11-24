f5cbb2a953eb17fd88015b45de8b4ae3c95fcb3c
I am Roshik <Roshik9841@gmail.com>
2025-11-24 15:22:01 +0545

    Setup complete

---
+++ product-app/src/App.jsx
@@ -0,0 +1,9 @@
+import './index.css';
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-red-100">
+     <p> ASD asfsaf  </p> 
+      </div>
+  )
+}
